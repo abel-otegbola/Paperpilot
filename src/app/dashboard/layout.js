543e4f9ccfd6856d562d7732d4ab5d492437a6bc
abel-otegbola <abel15655@gmail.com>
2023-07-02 20:02:54 +0100

    feat: recommendations schema and context [x] designed the schema for recommendations in the database [x] Incorporated the context api for the recommendations

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,14 +1,45 @@
 'use client'
 import DashboardSidebar from "@/components/dashboardSidebar/page";
+import axios from "axios";
 import { useSession } from "next-auth/react"
+import { createContext, useEffect, useState } from "react"
+
+export const UserContext = createContext()
 
 export default function RootLayout({ children }) {
     const {data:session} = useSession();
+    const [userData, setUserData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+
+    const getData = async () => {
+        const res = await axios.get(`/api/getRecommendations/${session?.user.email}`)
+        // Recommendation: handle errors
+        .catch(err => {
+            setError('Failed to fetch data')
+            setLoading(false)
+            setError("")
+            throw new Error("Failed to fetch data")
+        })
+        setLoading(false)
+        return setUserData(res.data)
+    }
+
+    useEffect(() => {
+        if(session) {
+            getData()
+        }
+        else {
+            setLoading(true)
+        }
+    }, [session])
 
     return (
-        <div className="flex min-h-[90vh] gap-2 bg-slate-100 dark:bg-slate-100/[0.05]">
-            <DashboardSidebar session={session}/>
-            <div className="flex-1">{ children }</div>
-        </div>
+        <UserContext.Provider value={{userData, loading, error}}>
+            <div className="flex min-h-[90vh] gap-2 bg-slate-100 dark:bg-slate-100/[0.05]">
+                <DashboardSidebar session={session}/>
+                <div className="flex-1">{ children }</div>
+            </div>
+        </UserContext.Provider>
     )
 }
--- src/model/Schema.js
+++ src/model/Schema.js
@@ -4,12 +4,20 @@ const userSchema = new Schema({
     fullname: String,
     email: String,
     password: String,
-    recommendations: Array,
-    papers: Array
+}, {
+    timestamps: true
+})
+
+const recommendationsSchema = new Schema({
+    subjects: Array,
+    platforms: Array,
+    time: Array,
+    user: String
 }, {
     timestamps: true
 })
 
 const Users = models.user || model("user", userSchema);
+export const Recommendations = models.recommendation || model("recommendation", recommendationsSchema)
 
 export default Users;
